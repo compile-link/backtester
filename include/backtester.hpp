e0e8d20e8690b2c169ef15010bedcebd18255035
compileit <compileit@protonmail.com>
2025-10-27 23:18:08 +0100

    Implement Backtester skeleton with placeholder run() method

---
+++ include/backtester.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+class Backtester {
+    public:
+        void run() {
+            std::cout << "Backtester started\n";
+
+            std::cout << "Bactester finished\n";
+        }
+};
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "backtester.hpp"
+
+int main() {
+    Backtester backtester;
+    backtester.run();
+    return 0;
+}
