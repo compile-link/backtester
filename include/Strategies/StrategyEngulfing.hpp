aed780be4b24e92ba67378bd5f99f362612e6f8b
compileit <compileit@protonmail.com>
2026-02-05 23:27:50 +0100

    Implement onCandle method for handling consecutive data for StrategyEngulfing

--- include/Strategies/StrategyEngulfing.hpp
+++ include/Strategies/StrategyEngulfing.hpp
@@ -2,9 +2,28 @@
 
 #include "Strategy.hpp"
 
+enum class MoveDirection {
+    Bull,
+    Bear
+};
+
+struct StrongMove {
+    StrongMove(size_t minLen): minLength(minLen) {}
+
+    const size_t minLength;
+    std::vector<Candle> candles;
+    MoveDirection direction;
+    Candle pivotCandle;
+
+    
+    bool isComplete() const noexcept { return (candles.size() >= minLength); }
+    bool isEngulfing(const Candle& candle) const noexcept;
+    void update(const Candle& candle);
+};
+
 class StrategyEngulfing : public Strategy {
     public:
-        StrategyEngulfing() noexcept {}
+        StrategyEngulfing(size_t minLength = kMoveMinLength) noexcept: strongMove_(minLength) {}
         Signal onCandle(const Candle& candle) override; 
         
         // Strategy name enabling polymorphism
@@ -18,4 +37,8 @@ class StrategyEngulfing : public Strategy {
 
         // Strategy description can be called on class
         static void staticDescription() noexcept;
+        
+    private:
+        static const size_t kMoveMinLength = 2;
+        StrongMove strongMove_;
 };
--- src/Strategies/StrategyEngulfing.cpp
+++ src/Strategies/StrategyEngulfing.cpp
@@ -3,7 +3,24 @@
 #include <iostream>
 
 Signal StrategyEngulfing::onCandle(const Candle& candle) {
-    return Signal::Wait;
+    Signal signal = Signal::Wait;
+    if(strongMove_.isComplete() && strongMove_.isEngulfing(candle)) {
+        switch (strongMove_.direction) {
+            case MoveDirection::Bull: 
+                signal = Signal::Sell;
+                break;
+            case MoveDirection::Bear: 
+                signal = Signal::Buy;
+                break;
+            default:
+                signal = Signal::Wait;
+                break;
+        }
+    };
+
+    strongMove_.update(candle);
+
+    return signal;
 } 
 
 void StrategyEngulfing::staticDescription() noexcept {
@@ -11,3 +28,32 @@ void StrategyEngulfing::staticDescription() noexcept {
 Buy signal: bullish engulfing candle.\n\
 Sell signal: bearish engulfing candle.\n";
 } 
+
+void StrongMove::update(const Candle& candle) {
+    MoveDirection candleDirection = (candle.open < candle.close) ? MoveDirection::Bull : MoveDirection::Bear;   
+
+    if (candles.empty()) {
+        this->direction = candleDirection;
+    };
+
+    if (this->direction != candleDirection) {
+        candles.clear();
+        this->direction = candleDirection;
+    };
+
+    candles.push_back(candle);
+}
+
+bool StrongMove::isEngulfing(const Candle& candle) const noexcept {
+    if (candles.empty()) return false;
+    const Candle& prev = candles.back();
+
+    MoveDirection candleDirection = (candle.open < candle.close) ? MoveDirection::Bull : MoveDirection::Bear;   
+    
+    if(candleDirection == this->direction) return false;
+
+    bool isEngulfing = (candle.open <= prev.close && candle.close >= prev.open) ||
+                       (candle.open >= prev.close && candle.close <= prev.open);
+        
+    return isEngulfing;
+}
